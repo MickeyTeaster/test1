65366f9e1d2dd4340cdaf40075474c8d6dc9499a
MickeyTeaster <mickey.goodbye@gmail.com>
2026-02-18 17:59:38 +0200

    refactor: separate login data and add helper

---
+++ tests/loginData.ts
@@ -0,0 +1,4 @@
+export const loginData = [
+    { user: 'tomsmith', pass: 'SuperSecretPassword!', expected: 'secure area' },
+    { user: 'invalidUser', pass: 'invalidPass', expected: 'invalid' }
+];
